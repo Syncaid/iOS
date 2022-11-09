d29a4c52b947cce5412a88c22ff08d0ef2f204dc
ParanoidJohnny <ahmedgazez@gmail.com>
2022-11-09 10:14:36 +0100

    initialll

---
+++ syncaid/syncaidApp.swift
@@ -0,0 +1,17 @@
+//
+//  syncaidApp.swift
+//  syncaid
+//
+//  Created by AhmedFatma on 9/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct syncaidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
